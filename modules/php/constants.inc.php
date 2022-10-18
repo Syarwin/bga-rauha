70d5cac6aa95b9d0ac30f70f5b61a3ee23fdecc3
Emmanuel ALBISSER <firgon@hotmail.com>
2022-10-18 13:23:08 +0200

    constants ok

--- modules/php/constants.inc.php
+++ modules/php/constants.inc.php
@@ -17,17 +17,17 @@ const OPTION_CONFIRM_ENABLED = 2;
  */
 const ST_GAME_SETUP = 1;
 
-const ST_NEXT_ROUND = 2
-const ST_MOVE_AVATARS = 3
+const ST_NEXT_ROUND = 2;
+const ST_MOVE_AVATARS = 3;
 
-const ST_CHOOSE_BIOME = 4
-const ST_NEXT_PLAYER = 5
-const ST_PLACE_BIOME = 6
-const ST_HOST_GOD = 7
-const ST_ACT_BIOMES = 8
+const ST_CHOOSE_BIOME = 4;
+const ST_NEXT_PLAYER = 5;
+const ST_PLACE_BIOME = 6;
+const ST_HOST_GOD = 7;
+const ST_ACT_BIOMES = 8;
 
-const ST_COUNT_NEXT_PLAYER = 9
-const ST_COUNT_ACTION = 10
+const ST_COUNT_NEXT_PLAYER = 9;
+const ST_COUNT_ACTION = 10;
 
 const ST_PRE_END_OF_GAME = 98; //TODO but why ?
 const ST_END_GAME = 99;
@@ -47,8 +47,8 @@ const TRANSITIONS = [
     'end_turn' => ST_NEXT_ROUND,
     'count' => ST_COUNT_ACTION,
     'skip_count' => ST_COUNT_NEXT_PLAYER
-]
-	
+];
+
 /******************
  ****** STATS ******
  ******************/
